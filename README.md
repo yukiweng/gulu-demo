a5dfcb966ffdbc5c535ca94d3c8e4386d1c52f63
Yuki <yukiweng11@163.com>
2019-11-19 20:22:26 +0800

    ignore dist/ & .cache/

---
+++ README.md
@@ -0,0 +1,7 @@
+# 一个 Vue UI 组件
+
+## 安装
+使用本框架前，请在CSS中开启 border-box
+```
+*{box-sizing:border-box}
+```
---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+<button class="g-button">按钮</button>	
+</template>
+
+<script>
+	export default {
+		name:'g-button'
+	}
+</script>
+
+<style lang="scss">
+	.g-button{
+		height: var(--button-height);
+		font-size: var(--font-size);
+		padding:0 1em;
+		color:var(--color);
+		background: var(--button-bg);
+		border:1px solid var(--border-color);
+		border-radius: var(--border-radius);
+		&:hover{
+			border-color:var(--border-hover-color);
+		};
+		&:active{
+			background: var(--button-active-bg);
+		} 
+
+	}
+
+</style>
