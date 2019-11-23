77a5727585b70d93991c3fb69d099a54fc24d992
Yuki <yukiweng11@163.com>
2019-11-23 18:11:24 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export Button from '/src/button'
-export ButtonGroup from '/src/button-group'
-export Icon from '/src/icon'
+import Button from '/src/button'
+import ButtonGroup from '/src/button-group'
+import Icon from '/src/icon'
 
 export {Button,ButtonGroup,Icon}
