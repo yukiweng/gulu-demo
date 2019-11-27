4e80d1a529939e6a1cc86fe23ee71e359c7e2426
Yuki <yukiweng11@163.com>
2019-11-27 17:39:51 +0800

    基本实现row和col

---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col">
+        <slot></slot>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+    .col {
+        border: 1px solid green;
+        background: grey;
+        width: 50%;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
+            }
+        }
+
+    }
+</style>
---
+++ src/row.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row" :class="gutter && `gutter-${gutter}`">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'g-row',
+        props: ['gutter']
+    }
+
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+        height: 100px;
+        $class-prefix: gutter-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                & > *:not(:last-child) {
+                    margin-right: #{$n}px
+                }
+
+            }
+        }
+    }
+</style>
