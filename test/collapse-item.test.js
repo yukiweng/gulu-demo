b071a2d8b80fff95e9b611455a721f5e57b9f01a
Yuki <yukiweng11@163.com>
2019-12-10 16:40:51 +0800

    添加collapse/collapse-item两个测试脚本

---
+++ test/collapse-item.test.js
@@ -0,0 +1,34 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import CollapseItem from '../src/collapse-item'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('CollapseItem', () => {
+    it('存在.', () => {
+        expect(CollapseItem).to.exist
+    })
+    describe('props', () => {
+        const Constructor=Vue.extend(CollapseItem)
+        let vm
+        afterEach(()=>{vm.$destroy()})
+        it('接收selected', () => {
+            vm=new Constructor({
+                propsData:{
+                    title:'标题'
+                }
+            }).$mount()
+            expect(vm.$refs.title.innerHTML.indexOf('标题')>=0).to.be.true
+        })
+        it('接收name', () => {
+
+            vm=new Constructor({
+                propsData:{
+                    name:'test'
+                }
+            }).$mount()
+            expect(vm.$el.getAttribute('name')).to.eq('test')
+        })
+    })
+})
