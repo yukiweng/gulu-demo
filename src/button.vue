645ab9fe99b67d2b94f8e4c242d1f88a500f406a
Yuki <yukiweng11@163.com>
2019-12-10 20:11:04 +0800

    修改button样式

--- src/button.vue
+++ src/button.vue
@@ -31,29 +31,37 @@
 </script>
 
 <style lang="scss" scoped>
+        $button-height: 32px;
+        $font-size: 14px;
+        $color: #333;
+        $button-bg: #fff;
+        $button-active-bg: #eee;
+        $border-radius: 4px;
+        $border-color: #999;
+        $border-hover-color: #666;
     @keyframes spin {
         0%{tranform:rotate(0deg)}
          100%{transform:rotate(360deg)}
     }
     .g-button {
-        height: var(--button-height);
-        font-size: var(--font-size);
+        height: $button-height;
+        font-size: $font-size;
         padding: 0 1em;
-        color: var(--color);
-        background: var(--button-bg);
-        border: 1px solid var(--border-color);
-        border-radius: var(--border-radius);
+        color: $color;
+        background: $button-bg;
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
         display:inline-flex;
         justify-content: center;
         align-items:center;
         vertical-align: middle;
 
         &:hover {
-            border-color: var(--border-hover-color);
+            border-color: $border-hover-color;
         } ;
 
         &:active {
-            background: var(--button-active-bg);
+            background: $button-active-bg;
         };
         >.icon{order:1;margin-right: 0.5em};
         > .content{order:2};
