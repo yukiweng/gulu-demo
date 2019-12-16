86459c642a882827d49158762e78c522ba709739
Yuki <yukiweng11@163.com>
2019-12-16 13:12:39 +0800

    添加.npmignore

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 import Button from './src/button/button'
 import ButtonGroup from './src/button/button-group'
 import Icon from './src/icon'
-import Input from './src/input';
-import Row from './src/grid/row'
-import Col from './src/grid/col'
-import Layout from './src/layout/layout'
-import Sider from './src/layout/sider';
-import Header from './src/layout/header';
-import Content from './src/layout/content';
-import Footer from './src/layout/footer';
-import Toast from './src/toast/toast'
-import plugin from './src/toast/plugin';
-import Tabs from './src/tabs/tabs'
-import TabsHead from './src/tabs/tabs-head'
-import TabsItem from './src/tabs/tabs-item'
-import TabsBody from './src/tabs/tabs-body'
-import TabsPane from './src/tabs/tabs-pane'
-import Popover from './src/popover'
-import Collapse from './src/collapse/collapse'
-import CollapseItem from './src/collapse/collapse-item'
 
-export {Button,ButtonGroup,Icon,Input,Row,Col,Layout,Sider,Header,Content,Footer,Toast,plugin,Tabs,TabsHead,TabsItem,TabsBody,TabsPane,Popover,Collapse,CollapseItem}
+
+export {Button,ButtonGroup,Icon}
